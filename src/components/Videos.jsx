427ce22392d9781a9fd5eda7255f6d1cb0044932
Techkon08 <maheshk86000@gmail.com>
2023-07-01 22:51:42 +0530

    issue

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Stack, Box } from "@mui/material";
+import { Stack, Box } from '@mui/material';
+import {VideoCard,channelCard} from './';
 
-const Videos = () => {
+
+const Videos = ({ videos = [] }) => {
   return (
-    <div>Videos</div>
-  )
-}
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <channelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
 
-export default Videos
+export default Videos;
